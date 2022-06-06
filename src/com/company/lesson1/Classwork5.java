40cc6fcd1fd79dc3c48b09eec53b83bf0d5b4729
Yury0304 <frol3492@gmail.com>
2022-06-07 01:41:11 +0300

    Вывод сообщения Hello + <Имя пользователя>

---
+++ src/com/company/lesson1/Classwork5.java
@@ -0,0 +1,11 @@
+package com.company.lesson1;
+import java.util.Scanner;
+
+public class Classwork5 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите имя пользователя");
+        String str = s.nextLine();
+        System.out.println("Hello "+str);
+    }
+}
