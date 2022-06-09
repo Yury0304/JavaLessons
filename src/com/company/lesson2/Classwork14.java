21694fbb6decb1cb70f27ef6b3d377e16543796e
Yury0304 <frol3492@gmail.com>
2022-06-09 12:50:51 +0300

    Display numbers 0 -5 -10 -15 -20 -25 -30 -35 -40 -45

---
+++ src/com/company/lesson2/Classwork14.java
@@ -0,0 +1,14 @@
+package com.company.lesson2;
+
+public class Classwork14 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 5;
+        int sum = 1;
+        while (sum <= 10) {
+            System.out.print(i + " ");
+            i = i - j;
+            sum++;
+        }
+    }
+}
