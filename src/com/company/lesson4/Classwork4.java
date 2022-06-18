e4b304236ccd2d04cf02fc03e778263edeb59417
Yury0304 <frol3492@gmail.com>
2022-06-18 13:29:16 +0300

    Sum of all members from two dim. array

---
+++ src/com/company/lesson4/Classwork4.java
@@ -0,0 +1,21 @@
+package com.company.lesson4;
+
+import java.util.Arrays;
+
+public class Classwork4 {
+    public static void main(String[] args) {
+        int[][] twoDimArray = new int[4][4];
+        int i, j, sum = 0;
+        for (i = 0; i < twoDimArray.length; i++) {
+            for (j = 0; j < twoDimArray.length; j++) {
+                twoDimArray[i][j] = ((int) (Math.random() * 10));
+                System.out.println(Arrays.deepToString(twoDimArray));
+                sum = sum + twoDimArray[i][j];
+            }
+        }
+        System.out.println("Here's your random array");
+        System.out.println(Arrays.deepToString(twoDimArray));
+        System.out.println("Sum of all members");
+        System.out.println(sum);
+    }
+}
