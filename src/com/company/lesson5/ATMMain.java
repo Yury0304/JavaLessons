3ba453bfab5eda657eb8ad693c1eadf80977e38a
Yury0304 <frol3492@gmail.com>
2022-06-23 15:54:43 +0300

    ATM main app

--- src/com/company/lesson5/ATMMain.java
+++ src/com/company/lesson5/ATMMain.java
@@ -2,10 +2,9 @@ package com.company.lesson5;
 
 public class ATMMain {
     public static void main(String[] args) {
-        ATM Atm1 = new ATM(1,2,3);
-
-        Atm1.chargeATM(1,0,0);
-        Atm1.withdrawATM(100);
-        Atm1.currentATMInfo();
+        ATM Atm1 = new ATM(1, 2, 3);
+        Atm1.showCurrentATMInfo();
+        Atm1.chargeATM(170);
+        Atm1.withdrawATM(170);
     }
 }
