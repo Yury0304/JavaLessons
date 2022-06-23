bba226c3afd8191ead3b4cc4d6b859b8f189c32d
Yury0304 <frol3492@gmail.com>
2022-06-23 15:56:35 +0300

    HDD class with methods

--- src/com/company/lesson5/HDD.java
+++ src/com/company/lesson5/HDD.java
@@ -1,12 +1,12 @@
 package com.company.lesson5;
 
 public class HDD {
-    String brand;
-    int size;
-    String type;
+    private String brand;
+    private int size;
+    private String type;
 
     HDD() {
-        this.brand="Seagate";
+        this.brand = "Seagate";
         this.size = 500;
         this.type = "Inner";
     }
@@ -17,18 +17,19 @@ public class HDD {
         this.type = type;
     }
 
-    void HDDInfo() {
+    void showHDDInfo() {
         System.out.println("HDD brand is " + brand);
-        System.out.println("HDD size is " + size+"Gb");
+        System.out.println("HDD size is " + size + "Gb");
         System.out.println("HDD type is " + type);
+        System.out.println();
     }
 
     @Override
     public String toString() {
         return "HDD{" +
-                "brand='" + brand + '\'' +
-                ", size=" + size +
-                ", type='" + type + '\'' +
+                "brand '" + brand + '\'' +
+                ", size " + size +
+                ", type '" + type + '\'' +
                 '}';
     }
 }
