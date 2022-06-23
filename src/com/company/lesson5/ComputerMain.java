7f10e24a1045393243cb1f24ff0d8dd42886fb6a
Yury0304 <frol3492@gmail.com>
2022-06-23 15:55:31 +0300

    Computer main app

--- src/com/company/lesson5/ComputerMain.java
+++ src/com/company/lesson5/ComputerMain.java
@@ -2,13 +2,13 @@ package com.company.lesson5;
 
 public class ComputerMain {
     public static void main(String[] args) {
-        RAM ram4Gb = new RAM("Samsung", 4);
-        HDD hdd500Gb = new HDD("Seagate", 500, "inner");
-        Computer M2 = new Computer(1000, "Acer", ram4Gb, hdd500Gb);
+        RAM RAM4Gb = new RAM("Samsung", 4);
+        HDD HDD500Gb = new HDD("Seagate", 500, "inner");
+        Computer M2 = new Computer(1000, "Acer", RAM4Gb, HDD500Gb);
         Computer M1 = new Computer(2000, "Asus");
         System.out.println("Computer M1");
-        M1.ComputerInfoShort();
+        M1.showComputerInfoShort();
         System.out.println("Computer M2");
-        M2.ComputerInfoLong();
+        M2.showComputerInfoLong();
     }
 }
