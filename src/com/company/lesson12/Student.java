dc534c36ba9494adcfe53f328391fd553bfe15e7
Yury0304 <frol3492@gmail.com>
2022-07-14 21:18:22 +0300

    Student class

--- src/com/company/lesson12/Student.java
+++ src/com/company/lesson12/Student.java
@@ -7,14 +7,30 @@ import java.util.Map;
 
 public class Student {
 
-    String name;
-    float mark;
+   private String name;
+   private float mark;
 
     public Student() {
     }
 
     public Student(String name, float mark) {
         this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getMark() {
+        return mark;
+    }
+
+    public void setMark(float mark) {
         this.mark = mark;
     }
 
